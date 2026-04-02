97d857bd8cda47aef6058160eb2bba4a2946bad6
corjqnrl <coqnrl@ewha.ac.kr>
2026-04-02 18:25:44 +0900

    feat: member controller 생성

---
+++ community/src/main/java/efub/assignment/community/member/controller/MembersController.java
@@ -0,0 +1,53 @@
+package efub.assignment.community.member.controller;
+
+import efub.assignment.community.member.dto.request.CreateMemberRequestDto;
+import efub.assignment.community.member.dto.request.ProfileUpdateRequestDto;
+import efub.assignment.community.member.dto.response.CreateMemberResponseDto;
+import efub.assignment.community.member.dto.response.MemberResponseDto;
+import efub.assignment.community.member.service.MembersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MembersController {
+
+    private final MembersService membersService;
+
+    // 회원 조회: GET /members/{memberId}
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberResponseDto> getMember(@PathVariable("memberId") Long memberId) {
+        MemberResponseDto responseDto = membersService.getMember(memberId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    // 회원 가입: POST /members
+    @PostMapping
+    public ResponseEntity<CreateMemberResponseDto> createMember(@RequestBody CreateMemberRequestDto requestDto) {
+        CreateMemberResponseDto responseDto = membersService.createMember(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    // 회원 프로필 수정: PATCH /members/profile/{memberId}
+    @PatchMapping("/profile/{memberId}")
+    public ResponseEntity<MemberResponseDto> updateProfile(@PathVariable("memberId") Long memberId,
+                                                           @RequestBody ProfileUpdateRequestDto requestDto) {
+        MemberResponseDto responseDto = membersService.updateProfile(memberId, requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    // 회원 삭제
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<Map<String, String>> deleteMember(@PathVariable("memberId") Long memberId) {
+        membersService.deleteMember(memberId);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "성공적으로 탈퇴되었습니다.");
+        return ResponseEntity.ok(response);
+    }
+}
